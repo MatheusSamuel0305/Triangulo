a51ab47f9db7f5f23674af10534adefb3ede16a9
MatheusSamuel0305 <Matheussamuel05@gmail.com>
2024-11-28 15:25:21 -0300

    first commit

---
+++ Triangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Digite o valor de X: ");
+        double x = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de Y: ");
+        double y = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de Z: ");
+        double z = double.Parse(Console.ReadLine());
+
+        if (x < y + z && y < x + z && z < x + y) {
+            if (x == y && y == z)
+                Console.WriteLine("Triângulo Equilátero");
+            else if (x == y || x == z || y == z)
+                Console.WriteLine("Triângulo Isósceles");
+            else
+                Console.WriteLine("Triângulo Escaleno");
+        }
+        else {
+            Console.WriteLine("Os valores não formam um triângulo");
+        }
+    }
+}
